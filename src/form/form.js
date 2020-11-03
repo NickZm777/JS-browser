25bf6a83f952bc4af04b731363ef13d12953353b
Nick <lacoste777v@gmail.com>
2020-11-03 21:44:54 +0300

    Collecting data from form

--- src/form/form.js
+++ src/form/form.js
@@ -1,8 +1,29 @@
+/* eslint-disable func-names */
+/* eslint-disable object-shorthand */
+/* eslint-disable prefer-const */
 /* eslint-disable no-console */
+
+// вариант с массивом (кривоватый:))
 const userData = function () {console.log(Array.from(document.getElementsByTagName(`input`), e => e.value))};
-
-
 const btnSubmit = document.querySelector('.submit');
+btnSubmit.addEventListener("click", userData)
 
-btnSubmit.addEventListener("click", userData);
 
+// вариант с объектом
+const form = document.forms[0]
+form.addEventListener('submit', (e) => sendData(e))
+
+function sendData(e) {
+    e.preventDefault()
+    const userDataObject = {
+        title: form.elements.title.value,
+        firstName: form.elements.firstName.value,
+        middleName: form.elements.middleName.value,
+        lastName: form.elements.lastName.value,
+        country: form.elements.country.value,
+        phone: form.elements.phone.value,
+        email: form.elements.email.value,
+        registration: form.elements.registration.checked,
+    }
+    console.log(userDataObject)
+}
